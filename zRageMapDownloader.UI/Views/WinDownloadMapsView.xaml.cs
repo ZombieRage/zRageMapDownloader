b452f41aeafad27c5b6dc2016eea0591ef80b959
Daniel <danielhidalgojunior@gmail.com>
2020-03-26 19:05:25 -0300

    Add project files.

---
+++ zRageMapDownloader.UI/Views/WinDownloadMapsView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace zRageMapDownloader.Views
+{
+    /// <summary>
+    /// Interaction logic for WinDownloadMapsView.xaml
+    /// </summary>
+    public partial class WinDownloadMapsView : Window
+    {
+        public WinDownloadMapsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
