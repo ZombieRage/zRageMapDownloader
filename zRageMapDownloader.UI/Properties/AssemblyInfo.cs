c4718ee86b084edc82ff6bb653fbc725d2125daf
Daniel <danielhidalgojunior@gmail.com>
2020-03-28 18:27:41 -0300

    version 1.1

--- zRageMapDownloader.UI/Properties/AssemblyInfo.cs
+++ zRageMapDownloader.UI/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Windows;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 //In order to begin building localizable applications, set
 //<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: Guid("0f6f7de9-ef9b-4ec9-b707-8dad9d1978aa")]
 [assembly: NeutralResourcesLanguage("en")]
