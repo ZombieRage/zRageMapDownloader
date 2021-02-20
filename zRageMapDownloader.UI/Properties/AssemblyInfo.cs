d58bd9fd1f3bca0eefc80690acaa16fa306fca27
Daniel Hidalgo <daniel.hidalgo@spcm.com.br>
2021-02-20 02:06:45 -0300

    Version updated to v1.1.3

--- zRageMapDownloader.UI/Properties/AssemblyInfo.cs
+++ zRageMapDownloader.UI/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Zombie Rage")]
 [assembly: AssemblyProduct("zRageMapDownloader")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.2.0")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
+[assembly: AssemblyVersion("1.1.3.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
 [assembly: Guid("0f6f7de9-ef9b-4ec9-b707-8dad9d1978aa")]
 [assembly: NeutralResourcesLanguage("en")]
